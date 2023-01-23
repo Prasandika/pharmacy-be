81d7b4ec23d7952230d4bb3ed493d8bb84307b74
Piyumi Prasandika <10749850@students.plymouth.ac.uk>
2023-01-24 00:02:50 +0530

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+import { ProductsModule } from './api/products/product.module';
+import { UsersModule } from './api/users/user.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './api/orders/orders.module';
+import { CartsModule } from './api/carts/carts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+    CartsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
